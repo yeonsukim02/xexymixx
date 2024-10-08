62cb129c6b12d21700e9d31aaa539addf888acf0
yeonsukim02 <gkfn3470@gmail.com>
2024-10-08 10:35:50 +0900

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -4,30 +4,43 @@ const visual_all = new Swiper('.visual_all', {
     loop: true,
     slidesPerView: 1,
     spaceBetween: 0,
-    autoplay:{delay:2000},
-
+    autoplay: {delay:5000},
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-
-    // And if we need scrollbar
+    // scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
     },
 });
-
+const rank_product_wrap = new Swiper(".rank_product_wrap",{
+    slidesPerView: 4.5,
+    spaceBetween: 50,
+    autoplay:{delay:2000},
+    loop: true,
+    // navigation
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
 const sport_product_all = new Swiper(".sport_product_all",{
     slidesPerView: 4,
-    spaceBetween: 0,
+    spaceBetween: 60,
     autoplay:{delay:2000},
-    loop:true,
+    loop: true,
 })
-
 const item_product_all = new Swiper(".item_product_all",{
     slidesPerView: 4,
-    spaceBetween: 0,
+    spaceBetween: 60,
     autoplay:{delay:2000},
-    loop:true,
+    loop: true,
+})
+const look_product_all = new Swiper(".look_product_all",{
+    slidesPerView: 4,
+    spaceBetween: 30,
+    autoplay:{delay:2000},
+    loop: true,
 })
